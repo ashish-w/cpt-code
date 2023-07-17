040ab2fb66272658c4449bbfdfb68b54a78ce8dd
Deepansh Dubey <56029566+deepanshdubey@users.noreply.github.com>
2023-07-18 03:06:14 +0530

    W capitalized

--- components/whatsAppButton/WhatsAppButton.js
+++ components/whatsAppButton/WhatsAppButton.js
@@ -4,7 +4,7 @@ const WhatsAppButton =  () => {
   return (
     <div className=''>
       <a href="https://api.whatsapp.com/send?phone=918130263311" target='_blank'>
-        <img className='whatsAppButton' src="whatsAppButton.svg" alt="whatsAppButton"/>
+        <img className='whatsAppButton' src="whatsAppButton.svg" alt="WhatsAppButton"/>
       </a>
     </div>
   )
